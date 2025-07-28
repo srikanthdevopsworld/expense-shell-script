e45666c0ee3161700e2611021dbfe2a7055d2a30
Srikanth <srikanthdevopsworld@gmail.com>
2025-07-28 21:28:21 +0530

    backend

--- backend.sh
+++ backend.sh
@@ -55,6 +55,8 @@ fi
 mkdir -p /app &>>$LOG_FILE_NAME
 VALIDATE $? "crearting app directory"
 
+rm -rf /app/*
+
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
 VALIDATE $? "Downloading backend"
 
